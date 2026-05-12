7f790a1d6e9bdeecf88f3da5b9c27d6a40794eaf
youeo <yaj5414@gmail.com>
2026-05-12 09:52:20 +0900

    feat: add lab 02

--- ch01/03/lab01/configaction.tf
+++ ch01/03/lab01/configaction.tf
@@ -13,6 +13,23 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+locals {
+  vpc_cidr_block = "10.0.0.0/24"
+}
+
+
+resource "aws_vpc" "main" {
+											# locals에 적힌 내용은 local로 표현
+  cidr_block           = local.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    # Naming 규칙 : organization project resource capability(용도)
+    Name = "tf-user-lab02-vpc-main"
+  }
+}
+
 resource "aws_security_group" "web1" {
   name = "tf-core-lab01-sg-web1"
 
