d3fb64b9fbe4c549f8e0750709b884c02eb0a76b
youeo <yaj5414@gmail.com>
2026-05-19 10:29:36 +0900

    Revert "ci: add ch07.lab01 tf"

--- ch07/lab01/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  org       = "tf-core-ej"
-  project   = "lab01"
-  namespace = "${local.org}-${local.project}"
-
-  message = "hello world from ${local.namespace}"
-}
--- ch07/lab01/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "message" {
-  value = local.message
-}
