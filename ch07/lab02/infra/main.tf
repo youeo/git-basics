9a63366bb8c69a8640efcb2b2c586668bacf4def
youeo <yaj5414@gmail.com>
2026-05-19 14:53:50 +0900

    ci: test validation fail

--- ch07/lab02/infra/main.tf
+++ ch07/lab02/infra/main.tf
@@ -1,17 +1,9 @@
 resource "aws_vpc" "this" {
-  cidr_block           = local.vpc.cidr_block
-  enable_dns_support   = local.vpc.enable_dns_support
-  enable_dns_hostnames = local.vpc.enable_dns_hostnames
+  cidr_block           = vpc.cidr_block
+  enable_dns_support   = vpc.enable_dns_support
+  enable_dns_hostnames = vpc.enable_dns_hostnames
 
   tags = {
     Name = "${local.namespace}-vpc-${local.vpc.name}"
-  }
-}
-
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "${local.namespace}-igw"
   }
 }
