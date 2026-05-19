01dfcf83bdcedd65fd477a326fcdc9035ea33174
youeo <yaj5414@gmail.com>
2026-05-19 15:00:44 +0900

    ci: test validation fail

--- ch07/lab02/infra/main.tf
+++ ch07/lab02/infra/main.tf
@@ -7,3 +7,11 @@ resource "aws_vpc" "this" {
     Name = "${local.namespace}-vpc-${local.vpc.name}"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${local.namespace}-igw"
+  }
+}
